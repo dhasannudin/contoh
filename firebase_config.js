3bdab9ead4ae51f974d5c55aec7130878ae402fc
dhasannudin <50048249+dhasannudin@users.noreply.github.com>
2025-08-14 12:59:41 +0700

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,23 +1,15 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase_config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCq1FWz6Jprping2rdePCs4MsnDtRnf20Q",
+const firebaseConfig = {
+    apiKey: "AIzaSyCqlFMZ6Jprping2rdePGs4MsnDtRnf200",
     authDomain: "alumina-kijing.firebaseapp.com",
     projectId: "alumina-kijing",
     storageBucket: "alumina-kijing.firebasestorage.app",
     messagingSenderId: "1039812060235",
-    appId: "1:1039812060235:web:4017fdf554663815fc59ce",
-    measurementId: "G-RC8MVBDTD1"
-  };
+    appId: "1:1039812060235:web:4017fdf554663815fc59ce"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
